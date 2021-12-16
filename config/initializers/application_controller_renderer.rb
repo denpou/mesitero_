2fac81bfaec4325a877b2d5c7bb138d70dfa59e9
denpou <ec2-user@ip-172-31-11-34.ec2.internal>
2021-12-16 03:29:38 +0000

    追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd5abd72aa192704b5536feb3cc4f7a7ca14fc649511515d2b93b7cfe79daa2a0843f10c95e013cfeb9baf3b8514b964c193943a72414c9e2db43e896785f6065'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_023523) do
+ActiveRecord::Schema.define(version: 2021_12_16_024707) do
+
+  create_table "resipis", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "image_id"
+    t.integer "user_id"
+    t.text "material", null: false
+    t.text "introduction", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

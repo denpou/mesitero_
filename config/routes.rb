c6a4a7e5a6a352e8d55dd88104cad365ed6be78e
denpou <ec2-user@ip-172-31-11-34.ec2.internal>
2021-12-16 04:22:48 +0000

    追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :resipis do
+    resource :favorites, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_040432) do
+ActiveRecord::Schema.define(version: 2021_12_16_042229) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment", null: false
+    t.integer "user_id"
+    t.integer "resipi_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "resipi_id"
     t.datetime "created_at", null: false

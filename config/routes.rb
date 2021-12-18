d8bc6d0bd58de2dee3f4a83c8fb7c471e1fea3bc
denpou <ec2-user@ip-172-31-11-34.ec2.internal>
2021-12-18 15:34:04 +0000

    追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
   devise_for :users
   root to: 'homes#top'
   resources :resipis do
     resource :favorites, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_042229) do
+ActiveRecord::Schema.define(version: 2021_12_17_033523) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_12_16_042229) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
